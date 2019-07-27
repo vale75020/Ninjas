03d8f80a389e435e750d5e24e0dbd90df17774dc
vale75020 <valentinosuma@hotmail.com>
2019-07-27 13:39:14 +0200

    conditionale rendering

--- src/components/Ninjas.js
+++ src/components/Ninjas.js
@@ -1,22 +1,51 @@
-import React from 'react'
+import React from "react";
+
+// const Ninjas = (props) => {
+
+//   const { ninjas } = props;
+//   const ninjaList = ninjas.map(ninja => {
+//       if (ninja.age > 20) {
+//       return (
+//       <div className="ninja" key={ninja.id}>
+//       <div>Name: {ninja.name}</div>
+//       <div>Age: {ninja.age}</div>
+//       <div>Belt: {ninja.belt}</div>
+//   </div>
+//   ) }
+//   else return null
+//     })
+//   return <div className="ninja-list">{ninjaList}</div>;
+// };
 
 const Ninjas = ({ninjas}) => {
-        
-        //const { ninjas } = props;
-        const ninjaList = ninjas.map(ninja => {
-            return (
-            <div className="ninja" key={ninja.id}>
-            <div>Name: {ninja.name}</div>
-            <div>Age: {ninja.age}</div>
-            <div>Belt: {ninja.belt}</div>
-        </div>
-        )
-          })
-        return (
-            <div className="ninja-list">
-                {ninjaList}
-            </div>
-        )
-}
+  const ninjaList = ninjas.map(ninja => {
+    return ninja.age > 20 ? (
+      <div className="ninja" key={ninja.id}>
+        <div>Name: {ninja.name}</div>
+        <div>Age: {ninja.age}</div>
+        <div>Belt: {ninja.belt}</div>
+      </div>
+    ) : null;
+  });
+  return <div className="ninja-list">{ninjaList}</div>;
+};
 
-export default Ninjas
+
+// const Ninjas = ({ninjas}) => {
+  
+//   return <div className="ninja-list">
+//       { ninjas.map(ninja => {
+//     return ninja.age > 20 ? (
+//       <div className="ninja" key={ninja.id}>
+//         <div>Name: {ninja.name}</div>
+//         <div>Age: {ninja.age}</div>
+//         <div>Belt: {ninja.belt}</div>
+//       </div>
+//     ) : null;
+//   })
+// }
+//   </div>;
+// };
+
+export default Ninjas;
+
