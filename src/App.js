b8fe5b750342892678803ec6fa29513f82445b3b
vale75020 <valentinosuma@hotmail.com>
2019-07-29 11:26:04 +0200

    lifecycle logs

--- src/App.js
+++ src/App.js
@@ -29,6 +29,16 @@ export default class App extends Component{
       ninjas // ninjas: ninjas
     })
   }
+
+  componentDidMount() {
+    console.log("component mounted")
+  }
+
+  componentDidUpdate(prevProps,prevState ) {
+    console.log("component updated")
+    console.log(prevProps, prevState)
+    
+  }
 render() {
   return (
     <div className="App">
